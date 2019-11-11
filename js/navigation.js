9b4de9b638733ca57a5864486daf02a6f7b8a025
Ana Rocha <apdsrocha@gmail.com>
2019-11-10 23:07:03 -0300

    added cart to mobile version

--- js/navigation.js
+++ js/navigation.js
@@ -2,17 +2,23 @@
   'use strict';
 
   let btnNavDesktop = doc.querySelector('.cart__counter-desktop');
+  let btnNavMobile = doc.querySelector('.cart__counter-mobile');
   let btnNavClose = doc.querySelector('.navigation-desktop__close-btn');
+
 
   let mainDishes = doc.querySelector('.main')
   let navDesktop = doc.querySelector('.navigation-desktop');
+  let navMobile = doc.querySelector('.navigation-mobile');
 
   let open = function( main, navigation ) {
     if(navigation.classList.contains("active")) {
       navigation.classList.remove("active");
+      main.style.opacity = "1";
     }
-    navigation.classList.add("active");
-    main.style.opacity = "0.5";
+    else {
+      navigation.classList.add("active");
+      main.style.opacity = "0.5";
+    }
   }
 
   let close = function( main, navigation) {
@@ -22,5 +28,6 @@
 
   btnNavDesktop.addEventListener('click', function() { open( mainDishes, navDesktop) });
   btnNavClose.addEventListener('click', function() {close( mainDishes, navDesktop)})
-  
+  btnNavMobile.addEventListener('click', function() { open( mainDishes, navMobile) });
+
 })(document);
